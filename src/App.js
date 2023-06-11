c40360ab120dd11b283b08e237c8d4968b1a95d5
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-06-11 18:24:14 +0600

    banner add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./Components/navbar/Navbar";
+import Banner from "./Components/banner/Banner";
+import Features from "./Components/features/Features";
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText">
     <div className="max-w-screen-2xl mx-auto px-16">
     <Navbar/>
+    <Banner/>
+    <Features/>
     </div>
   </div>
   );
