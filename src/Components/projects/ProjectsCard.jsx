d41df51edd33f8a97f513bdeda04488fe1e08f0b
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-06-13 23:44:15 +0600

    Project  start

--- src/Components/projects/ProjectsCard.jsx
+++ src/Components/projects/ProjectsCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {BsGithub} from 'react-icons/bs'
+import {FaGlobe} from 'react-icons/fa'
 
 const ProjectsCard = ({title,des,src}) => {
   return (
@@ -7,6 +9,20 @@ const ProjectsCard = ({title,des,src}) => {
      hover:from-gray-900 hover:bg-gray-900 transition-colors duration-1000'>
        <div className='w-full h-[80%] overflow-hidden rounded-lg'>
         <img className='w-full  h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer' src={src} alt="" />
+       </div>
+       <div className='w-full mt-5'>
+
+        <div className='flex items-center justify-between'>
+        <h3>{title}</h3>
+        </div>
+          <span className='text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center
+           items-center text-gray-400 hover:text-designColor duration-1000 cursor-pointer'>
+            <BsGithub/>
+           </span>
+          <span className='text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center
+           items-center text-gray-400 hover:text-designColor duration-1000 cursor-pointer'>
+            <FaGlobe/>
+           </span>
        </div>
     </div>
   )
